0c163bcab211436dfae9b41ee25dda1b7362bfdb
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-28 11:49:03 +0200

    sbor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import Notiflix from "notiflix";
+
+
 axios.get('/users')
   .then(res => {
     console.log(res.data);
@@ -16,13 +18,34 @@ axios.get('/users')
 
 
 
+//   key - твій унікальний ключ доступу до API.
+//   q - термін для пошуку. Те, що буде вводити користувач.
+//   image_type - тип зображення. На потрібні тільки фотографії, тому постав значення photo.
+//   orientation - орієнтація фотографії. Постав значення horizontal.
+//   safesearch - фільтр за віком. Постав значення true.
+
+
+
+// webformatURL - посилання на маленьке зображення для списку карток.
+// largeImageURL - посилання на велике зображення.
+// tags - рядок з описом зображення. Підійде для атрибуту alt.
+// likes - кількість лайків.
+// views - кількість переглядів.
+// comments - кількість коментарів.
+// downloads - кількість завантажень.
+
+
+// "Sorry, there are no images matching your search query. Please try again."
+
+
+
 //   35827866-cac2bfdbcf92b350627521ced
 
 
-  https://pixabay.com/api/
+//   https://pixabay.com/api/
 
-  https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo
+//   https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo
 
-  https://pixabay.com/api/videos/
+//   https://pixabay.com/api/videos/
 
-  https://pixabay.com/api/videos/?key={ KEY }&q=yellow+flowers
+//   https://pixabay.com/api/videos/?key={ KEY }&q=yellow+flowers
